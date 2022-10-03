d87fb7d0291ccc1014cc885b00706a7724095455
jferni <75497458+jferni@users.noreply.github.com>
2022-10-03 02:47:13 +0200

    Add files via upload

---
+++ parte9/07-07-atributos.js
@@ -0,0 +1,11 @@
+let joaquin = {
+    nombre: "joaquin",
+    edad: "30",
+  };
+  
+  console.log(joaquin);
+  
+  joaquin.altura = "1,70";
+  
+  console.log(joaquin);
+  
---
+++ parte9/ejercicios-3-3.js
@@ -0,0 +1,13 @@
+let coche = {
+    Color: "Rojo",
+    Marca: "Ford",
+    Modelo: "Focus",
+    Motor: {
+      cilindros: 4,
+      capacidad: 2.2,
+    }
+  
+  };
+  
+  console.log(coche);
+  console.log(coche[Marca]);
